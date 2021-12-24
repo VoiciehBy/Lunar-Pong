2cb55ecd25025dcd6ccfee7fe33a42c0b8663797
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2021-12-24 13:14:03 +0100

    Update inputHandling.lua

--- inputHandling.lua
+++ inputHandling.lua
@@ -1,4 +1,4 @@
-function handleKeys(dt)
+function handlePaddlesControls(dt)
     if (love.keyboard.isDown("w") and (lPaddle.posY > 0)) then
         mv(lPaddle, 0, -PADDLE_SPEED * dt);
     elseif (love.keyboard.isDown("s") and (lPaddle.posY <= h - lPaddle.height)) then
@@ -10,6 +10,10 @@ function handleKeys(dt)
     elseif (love.keyboard.isDown("down") and (rPaddle.posY <= h - rPaddle.height)) then
         mv(rPaddle, 0, PADDLE_SPEED * dt);
     end
+end
+
+function handleKeys(dt)
+    handlePaddlesControls(dt);
 
     if (love.keyboard.isDown('p') and (Game.state == "Playing")) then
         Game.state = "Paused";
